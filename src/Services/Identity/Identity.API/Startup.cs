f8a1495ec14108b7e149ee0c95dc8807f4ebdef4
denisprokhorchik@hotmail.com <denisprokhorchik@hotmail.com>
2020-07-04 11:47:15 +0300

    renamed the basic namespace

--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Identity.API
+namespace O2.Identity.API
 {
     public class Startup
     {

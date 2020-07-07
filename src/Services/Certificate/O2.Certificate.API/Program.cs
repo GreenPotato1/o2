59d87e8602d986f58d3567ab89b9c64df60874ac
denisprokhorchik@hotmail.com <denisprokhorchik@hotmail.com>
2020-07-07 15:46:16 +0300

    added a new changes

--- src/Services/Certificate/O2.Certificate.API/Program.cs
+++ src/Services/Certificate/O2.Certificate.API/Program.cs
@@ -8,13 +8,16 @@ namespace O2.Certificate.API
 {
     public class Program
     {
+        public static readonly string Namespace = typeof(Program).Namespace;
+        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+        
         public static async Task Main(string[] args)
         {
             Console.WriteLine("============== O2 Certificate API =====================");
             var host = CreateHostBuilder(args).Build();
             await host.EnsureDbUpdateToDateUpdateAsync();
-            host.Run();
             Console.WriteLine("============== O2 Certificate API - state is started =====================");
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
